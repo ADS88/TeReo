985fe4eb4faedd29fe4147e23ada6de4e8789eb3
Andrew Sturman <ads92@uclive.ac.nz>
2019-09-10 00:28:22 +1200

    Added information bar

--- app/src/main/java/com/android/example/tereomaori/MainActivity.java
+++ app/src/main/java/com/android/example/tereomaori/MainActivity.java
@@ -5,10 +5,36 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.icon_about:
+                Intent intent = new Intent(this, InformationActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.icon_settings:
+                Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+
 
     }
 
